d825baf01e8cc718057f50815f3a428740792d79
PapiiCholoo <148296726+PapiiCholoo@users.noreply.github.com>
2026-01-21 23:15:58 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function redirectToServices() {
+  alert("Redirecting to LGU Services");
+}
+
+function redirectToAllServices() {
+  alert("Redirecting to All Services");
+}
+
+function redirectToAbout() {
+  alert("Redirecting to About Us");
+}
+
+window.addEventListener('resize', () => {
+  console.log(`Window resized to ${window.innerWidth}px`);
+});
